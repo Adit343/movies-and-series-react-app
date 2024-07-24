5e2d5f4dfa446325b9f8993c0dd0161903b9a3d2
Adit343 <shahadit68@gmail.com>
2024-07-24 23:05:02 +0530

    made some responsiveness changes to header component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,50 +1,45 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import logo from '../../Assets/images/video-player.png';
-const HeaderComponent = ()=>{
+
+const HeaderComponent = () => {
     const navData = [
-        {name:'Home', link:'/'},
-        {name:'Movies', link:'/movies'},
-        {name:'Tv Series', link:'/series'},
-        {name:'Popular',link:'/popular'},
-        {name:'UpComing', link:'/upcoming'},
-        {name:'Search', link:'/search'},
-       
-    ]
+        { name: 'Home', link: '/' },
+        { name: 'Movies', link: '/movies' },
+        { name: 'Tv Series', link: '/series' },
+        { name: 'Popular', link: '/popular' },
+        { name: 'UpComing', link: '/upcoming' },
+        { name: 'Search', link: '/search' },
+    ];
 
     return (
-        <header  className='header'>
-            <Navbar bg="dark" expand="lg" >
+        <header className='header'>
+            <Navbar bg="dark" expand="lg">
                 <Container>
                     <Navbar.Brand>
-                        <img src={logo} alt='' className='nav-logo'/></Navbar.Brand>
-                     <Navbar.Toggle aria-controls="navbarScroll" className='nav-color'  /> 
+                        <img src={logo} alt='' className='nav-logo' />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" className='nav-color' />
                     <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: '100px' }}
-                        navbarScroll
-                    >
-                        {
-                            navData.map((item)=>{
-                                return (
-                                    <Nav key={item.name}>
-                                        <Link to={item.link}>{item.name}</Link>
-                                    </Nav> 
-                                )
-                            })
-                        }
-                    </Nav>
+                        <Nav className="me-auto my-2 my-lg-0" navbarScroll>
+                            {
+                                navData.map((item) => {
+                                    return (
+                                        <Nav.Item key={item.name} className="nav-item">
+                                            <Link to={item.link} className="nav-link">{item.name}</Link>
+                                        </Nav.Item>
+                                    );
+                                })
+                            }
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-                    
         </header>
-    )
-}
+    );
+};
 
 export default HeaderComponent;
--- src/Container/Details/index.js
+++ src/Container/Details/index.js
@@ -1,79 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './details.css';
 import axios from 'axios';
 import DarkVariantExample from '../../Components/Carousel';
-import {img_300, img_not_available} from '../../Config';
+import { img_300, img_not_available } from '../../Config';
 
-const DetailsContainer = ()=>{
-
-    //params: this variable stores the parameters extracted from the URL using useParams().it likely contains information like 'movieid' and 'mediatype'
+const DetailsContainer = () => {
     const params = useParams();
-
-
-    //these are state variables which are used to store data fetched from external APIs.
     const [content, setContent] = useState();
     const [video, setVideo] = useState();
     const [credits, setCredits] = useState();
 
-    
-    const titleName =  content && content.name && content.name !== '' ? content.name : content && content.title && content.title !== '' ?  content.title : '';
-    
-
-    //extract values from 'params'
+    const titleName = content && content.name && content.name !== '' ? content.name : content && content.title && content.title !== '' ? content.title : '';
     const id = params.movieid || '';
-    const _media_type = params && params.mediatype &&  params.mediatype !== '' ? params.mediatype.toLowerCase() : '';
+    const _media_type = params && params.mediatype && params.mediatype !== '' ? params.mediatype.toLowerCase() : '';
     const API_KEY = process.env.REACT_APP_NOT_SECRET_CODE;
 
-
-    //these functions use Axios to fetch data from different endpoints of the movie database API
-
-    //fetches general info about the movie or TV show 
-    const fetchData = async () =>{
-        try{
-          const {data} = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}?api_key=${API_KEY}&language=en-US`);
-          setContent(data);
-        }catch(error){
-          console.error(error)
+    const fetchData = async () => {
+        try {
+            const { data } = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}?api_key=${API_KEY}&language=en-US`);
+            setContent(data);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-    //fetches video related data
-    const fetchVideo = async () =>{
-        try{
-          const {data} = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}/videos?api_key=${API_KEY}&language=en-US`);
-          setVideo(data.results[0]?.key);
-        }catch(error){
-          console.error(error)
+    const fetchVideo = async () => {
+        try {
+            const { data } = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}/videos?api_key=${API_KEY}&language=en-US`);
+            setVideo(data.results[0]?.key);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-    //fetches information about cast 
-    const creditsFetch = async ()=>{
-        try{
-          const {data} = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}/credits?api_key=${API_KEY}&language=en-US`);
-          setCredits(data.cast);
-          console.log('sdata',  data);
-        }catch(error){
-          console.error(error)
+    const creditsFetch = async () => {
+        try {
+            const { data } = await axios.get(`https://api.themoviedb.org/3/${_media_type}/${id}/credits?api_key=${API_KEY}&language=en-US`);
+            setCredits(data.cast);
+            console.log('sdata', data);
+        } catch (error) {
+            console.error(error);
         }
-    }
+    };
 
-
-    //useEffect is used to trigger the data fetching when component is mounted.
-    useEffect(()=>{
+    useEffect(() => {
         fetchData();
         fetchVideo();
         creditsFetch();
         //eslint-disable-next-line
-    }, [])
+    }, []);
 
-
-    //this function generates HTML content using the fetched data including the movie or TV show's title,image,rating and other details
-    const renderDataHtml = ()=>{
+    const renderDataHtml = () => {
         const ImageURL = content.poster_path ? img_300 + content.poster_path : img_not_available;
         const tagline = content.tagline || '';
         const vote_average = parseInt(content.vote_average);
@@ -82,16 +63,15 @@ const DetailsContainer = ()=>{
         const origin_country = content.origin_country && content.origin_country[0] ? content.origin_country[0] : content.production_countries && content.production_countries[0] && content.production_countries[0].name ? content.production_countries[0].name : '';
         const overview = content.overview;
         const first_air_date = content.first_air_date || content.release_date;
-        const  budget = content.budget || '';
-        const genres = content.genres && content.genres.length > 0 ? content.genres.map((item)=> <span  key={item.id}>{item.name}</span>) : '' ;
+        const budget = content.budget || '';
+        const genres = content.genres && content.genres.length > 0 ? content.genres.map((item) => <span key={item.id}>{item.name}</span>) : '';
+
         return (
             <Row>
                 <Col className='col-12'>
                     <h1>
-                        {titleName} 
-                        {
-                            tagline && tagline !== '' ? <small> {tagline}</small> : ''
-                        }
+                        {titleName}
+                        {tagline && tagline !== '' ? <small> {tagline}</small> : ''}
                     </h1>
                 </Col>
                 <Col className='col-12 col-xl-6'>
@@ -102,7 +82,6 @@ const DetailsContainer = ()=>{
                         <div className='card__content'>
                             <div className="card__wrap">
                                 <span className="card__rate"> {vote_average}</span>
-
                                 <ul className="card__list">
                                     <li>{original_language}</li>
                                     <li>{adult}</li>
@@ -110,20 +89,16 @@ const DetailsContainer = ()=>{
                             </div>
                             <ul className="card__meta">
                                 <li>
-                                    <span>Genre:</span> 
+                                    <span>Genre:</span>
                                     <span className='linkTag'>{genres}</span>
                                 </li>
                                 <li>
-                                    <span>Type:</span> 
+                                    <span>Type:</span>
                                     <span className='linkTag'>{_media_type}</span>
                                 </li>
-                                
                                 <li><span>Release year:</span> <span className='linkTag'>{first_air_date}</span></li>
-                                {
-                                    budget && budget !== '' ? <li><span>Budget:- </span>
-                                    <span className='linkTag'> {budget}</span></li> : ''
-                                }
-                                
+                                {budget && budget !== '' ? <li><span>Budget:- </span>
+                                    <span className='linkTag'> {budget}</span></li> : ''}
                                 <li><span>Country:</span> <span className='linkTag'>{origin_country}</span> </li>
                             </ul>
                             <div className="description_readmore_wrapper ">
@@ -134,39 +109,35 @@ const DetailsContainer = ()=>{
                 </Col>
                 <Col className='col-12 col-xl-6'>
                     <div className='frameSec'>
-                       
-                        <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <div className="responsive-video">
+                            <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        </div>
                     </div>
                 </Col>
             </Row>
-        )
-    }
+        );
+    };
+
     return (
         <>
             <main className='detailsPage'>
-            <Container>
-                {
-                    titleName && titleName !==  '' ? renderDataHtml() : 'Loading...'
-                }
-                
-            </Container>
-            <section className='section'>
-                <div  className='contentHead'>
-                    <Container>
-                        <Row>
-                            <Col className='col-12'>
-                                {
-                                    credits && credits.length > 0 ? <DarkVariantExample data={credits} /> : 'Loading data...'
-                                }
-                                
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            </section>
-        </main>
+                <Container>
+                    {titleName && titleName !== '' ? renderDataHtml() : 'Loading...'}
+                </Container>
+                <section className='section'>
+                    <div className='contentHead'>
+                        <Container>
+                            <Row>
+                                <Col className='col-12'>
+                                    {credits && credits.length > 0 ? <DarkVariantExample data={credits} /> : 'Loading data...'}
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </section>
+            </main>
         </>
-    )
-}
+    );
+};
 
 export default DetailsContainer;
